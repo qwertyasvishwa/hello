fa2568657f7b825962fdded47af11d509b9554df
Vishwa Raj <vishwa.raj@kalkine.com>
2024-02-01 16:51:39 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var msg = 'Hello World';
 console.log(msg);
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://anandhost:DYNGf7KKE0dGFs9Z@recluster.dawqed5.mongodb.net/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
